5deef09403d63461fdfb73104b5e82882db24685
Mohammad Arafath <mohammadarafath@Mohammads-MacBook-Pro.local>
2018-02-20 13:50:47 +0530

    Heaps Done

--- src/test/java/com/ironman/ma/Algorithms/Hashing/Core/FourSum/SolutionTest.java
+++ src/test/java/com/ironman/ma/Algorithms/Hashing/Core/FourSum/SolutionTest.java
@@ -5,6 +5,7 @@ import junit.framework.TestCase;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import static java.math.BigDecimal.ROUND_UNNECESSARY;
@@ -13,18 +14,50 @@ public class SolutionTest extends TestCase {
 
     public void testFourSum() {
 //        A : [ 36, 27, -35, 43, -15, 36, 42, -1, -29, 12, -23, 40, 9, 13, -24, -10, -24, 22, -14, -39, 18, 17, -21, 32, -20, 12, -27, 17, -15, -21, -48, -28, 8, 19, 17, 43, 6, -39, -8, -21, 23, -29, -31, 34, -13, 48, -26, -35, 20, -37, -24, 41, 30, 6, 23, 12, 20, 46, 31, -45, -25, 34, -23, -14, -45, -4, -21, -37, 7, -26, 45, 32, -5, -36, 17, -16, 14, -7, 0, 37, -42, 26, 28 ]
-        int[]arr=new int[]{36, 27, -35, 43, -15, 36, 42, -1, -29, 12, -23, 40, 9, 13, -24, -10, -24, 22, -14, -39, 18, 17, -21, 32, -20, 12, -27, 17, -15, -21, -48, -28, 8, 19, 17, 43, 6, -39, -8, -21, 23, -29, -31, 34, -13, 48, -26, -35, 20, -37, -24, 41, 30, 6, 23, 12, 20, 46, 31, -45, -25, 34, -23, -14, -45, -4, -21, -37, 7, -26, 45, 32, -5, -36, 17, -16, 14, -7, 0, 37, -42, 26, 28};
-        ArrayList<Integer> ints=new ArrayList<Integer>();
+        int[] arr = new int[]{36, 27, -35, 43, -15, 36, 42, -1, -29, 12, -23, 40, 9, 13, -24, -10, -24, 22, -14, -39, 18, 17, -21, 32, -20, 12, -27, 17, -15, -21, -48, -28, 8, 19, 17, 43, 6, -39, -8, -21, 23, -29, -31, 34, -13, 48, -26, -35, 20, -37, -24, 41, 30, 6, 23, 12, 20, 46, 31, -45, -25, 34, -23, -14, -45, -4, -21, -37, 7, -26, 45, 32, -5, -36, 17, -16, 14, -7, 0, 37, -42, 26, 28};
+        ArrayList<Integer> ints = new ArrayList<Integer>();
 //        B : [ 38, 34, -47, 1, 4, 49, -18, 10, 26, 18, -11, -38, -24, 36, 44, -11, 45, 20, -16, 28, 17, -49, 47, -48, -33, 42, 2, 6, -49, 30, 36, -9, 15, 39, -6, -31, -10, -21, -19, -33, 47, 21, 31, 25, -41, -23, 17, 6, 47, 3, 36, 15, -44, 33, -31, -26, -22, 21, -18, -21, -47, -31, 20, 18, -42, -35, -10, -1, 46, -27, -32, -5, -4, 1, -29, 5, 29, 38, 14, -22, -9, 0, 43 ]
-        int[]arr2=new int[]{38, 34, -47, 1, 4, 49, -18, 10, 26, 18, -11, -38, -24, 36, 44, -11, 45, 20, -16, 28, 17, -49, 47, -48, -33, 42, 2, 6, -49, 30, 36, -9, 15, 39, -6, -31, -10, -21, -19, -33, 47, 21, 31, 25, -41, -23, 17, 6, 47, 3, 36, 15, -44, 33, -31, -26, -22, 21, -18, -21, -47, -31, 20, 18, -42, -35, -10, -1, 46, -27, -32, -5, -4, 1, -29, 5, 29, 38, 14, -22, -9, 0, 43};
-        ArrayList<Integer> ints2=new ArrayList<Integer>();
-        for(int ae:arr){
+        int[] arr2 = new int[]{38, 34, -47, 1, 4, 49, -18, 10, 26, 18, -11, -38, -24, 36, 44, -11, 45, 20, -16, 28, 17, -49, 47, -48, -33, 42, 2, 6, -49, 30, 36, -9, 15, 39, -6, -31, -10, -21, -19, -33, 47, 21, 31, 25, -41, -23, 17, 6, 47, 3, 36, 15, -44, 33, -31, -26, -22, 21, -18, -21, -47, -31, 20, 18, -42, -35, -10, -1, 46, -27, -32, -5, -4, 1, -29, 5, 29, 38, 14, -22, -9, 0, 43};
+        ArrayList<Integer> ints2 = new ArrayList<Integer>();
+        for (int ae : arr) {
             ints.add(ae);
         }
-        for(int ae:arr2){
+        for (int ae : arr2) {
             ints2.add(ae);
         }
-        System.out.println(new Solution().solve(ints,ints2));
+//        System.out.println(new Solution().solve(ints,ints2));
+        System.out.println((int) (Math.log10(100) / (1.0 * Math.log10(2))));
+        System.out.println(((long) 1000000006 * 1000000006) % 1000000007);
 
+
+    }
+
+//49381776
+//    49381776
+
+
+    /**
+     Definition for binary tree
+     */
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+            left=null;
+            right=null;
+        }
+    }
+
+    class TreeNodeLev extends TreeNode{
+        int vLev;
+        public TreeNodeLev(int lev, TreeNode x){
+            super(x.val);
+            this.val = x.val;
+            this.left=x.left;
+            this.right=x.right;
+            this.vLev=lev;
+        }
     }
 }
